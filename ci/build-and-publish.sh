cb8ef10d8047480196f45cad05a436a3739e940a
kworam-rally <kevin.woram@broadcom.com>
2020-05-29 09:50:15 -0600

    calculate and use NEXT_BUILD_NUMBER

--- ci/build-and-publish.sh
+++ ci/build-and-publish.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
 # get next build number
-BUILD_NUMBER=$(curl http://repo-depot.f4tech.com/artifactory/rally-maven/com/rallydev/saml-utils-jar/maven-metadata.xml | sed -n 's:.*<latest>\(.*\)</latest>.*:\1:p')
-let BUILD_NUMBER+=1
+NEXT_BUILD_NUMBER=$(curl http://repo-depot.f4tech.com/artifactory/rally-maven/com/rallydev/saml-utils-jar/maven-metadata.xml | sed -n 's:.*<latest>\(.*\)</latest>.*:\1:p')
+let NEXT_BUILD_NUMBER+=1
+echo "NEXT_BUILD_NUMBER=${NEXT_BUILD_NUMBER}"
 
 # build and publish jar
 gw clean
-BUILD_NUMBER=testci gw shadowJar
-BUILD_NUMBER=testci gw publishSAMLJarPublicationToMavenRepository
+BUILD_NUMBER=${NEXT_BUILD_NUMBER} gw shadowJar
+BUILD_NUMBER=${NEXT_BUILD_NUMBER} gw publishSAMLJarPublicationToMavenRepository
